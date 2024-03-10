610afc249575da3145ac7c4f9481972ff140e283
komet0141 <90485651+komet0141@users.noreply.github.com>
2024-03-10 15:46:09 -0700

    Revert "make loaders throw exception"
    This reverts commit f702fc1bafc2ba6466a291b04e3ed4d48d500198.

--- src/main/java/io/github/komet0141/subclassCollector/utils/SubclassLoader.java
+++ src/main/java/io/github/komet0141/subclassCollector/utils/SubclassLoader.java
@@ -4,10 +4,14 @@ import io.github.komet0141.subclassCollector.CollectSubclass;
 import io.github.komet0141.subclassCollector.CollectionProcessor;
 
 public class SubclassLoader {
-    public static void load(String subpackageName) throws ClassNotFoundException {
-        Class.forName(fullClassName(subpackageName));
+    public static void load(String subpackageName) {
+        try {
+            Class.forName(fullClassName(subpackageName));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public static void load(Class<?> clazz) throws ClassNotFoundException {
+    public static void load(Class<?> clazz) {
         load(clazz.getAnnotation(CollectSubclass.OutputPackage.class).value());
     }
     public static String fullClassName(String subpackageName) {
